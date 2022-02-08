1ec637ee71dd0066a2408df3719085d908337b92
chemoul patrick <chemoulpatrick@chemouls-iMac.lan>
2022-02-08 15:51:18 -0800

    initial

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,30 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
 const About = () => {
     return ( 
 
 <div>
-        <header className={styles.header}>
-        <div className={styles.container}>
-            <div>
-                <h1>About</h1>
-                <p>Monitor team productivity and performance via automated tools: take screenshots and screen recordings, log team activity levels, monitor app and web use, and a lot more…</p>
-            </div>
-            <Image src="/images/myimg01.jpg" width="1000" height="667" alt="rcr" /> 
- 
-        </div>
-    </header>
+
+   <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="new description" />
+   </Head>
+
+   <header className={styles.header}>
+       <div className={styles.container}>
+           <div>
+               <h1>About</h1>
+               <p>Monitor team productivity and performance via automated tools: take screenshots and screen recordings, log team activity levels, monitor app and web use, and a lot more…</p>
+           </div>
+           <Image src="/images/myimg01.jpg" width="1000" height="667" alt="rcr" /> 
+
+       </div>
+   </header>
+
+
 </div>
 
      );
--- pages/contact.js
+++ pages/contact.js
@@ -1,16 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
 const Contact = () => {
     return ( 
 <div>
-        <header className={styles.header}>
-        <div className={styles.container}>
-            <div>
-                <h1>Contact</h1>
-                <p>Monitor team productivity and performance via automated tools: take screenshots and screen recordings, log team activity levels, monitor app and web use, and a lot more…</p>
-            </div>
-            <Image src="/images/myimg01.jpg" width="1000" height="667" alt="rcr" /> 
- 
-        </div>
-    </header>
+<Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="new description" />
+   </Head>
+
+   <header className={styles.header}>
+       <div className={styles.container}>
+           <div>
+               <h1>Contact</h1>
+               <p>Monitor team productivity and performance via automated tools: take screenshots and screen recordings, log team activity levels, monitor app and web use, and a lot more…</p>
+           </div>
+           <Image src="/images/myimg01.jpg" width="1000" height="667" alt="rcr" /> 
+
+       </div>
+   </header>
 </div>
      );
 }
