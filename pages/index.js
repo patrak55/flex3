d8cd1c13a90a72dcf44a574a3b5fc8955790bb5d
chemoul patrick <chemoulpatrick@chemouls-iMac.lan>
2022-02-08 15:17:18 -0800

    initial

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,13 @@ export default function Home() {
   return (
 
     <div>
+
+       <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="new description" />
+      </Head>
+
 <Navbar />
    
 
